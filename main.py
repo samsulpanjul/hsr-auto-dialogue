8a61927f9800310b7c088fc231b9cb24f8c0b4d4
Samsul <113889106+samsulpanjul@users.noreply.github.com>
2025-06-13 02:50:02 +0700

    fix: hsr won't continue dialogue

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+import os
 import tkinter as tk
 from tkinter import messagebox
 from pynput.keyboard import Controller, Listener, Key
@@ -5,24 +7,45 @@ import threading
 import time
 import random
 import pyautogui
+import pygetwindow as gw
 pyautogui.useImageNotFoundException(False)
 
 keyboard = Controller()
 running = False
 use_random_delay = False
-delay = 500
+delay = 100
 min_delay = 100
-max_delay = 1000
+max_delay = 500
 
+def resource_path(relative_path):
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
+
+def click(position):
+    hold_time = random.uniform(0.05, 0.1)
+    pyautogui.moveTo(position)
+    pyautogui.mouseDown()
+    time.sleep(hold_time)
+    pyautogui.mouseUp()
+
+icon = resource_path('./images/firefly.ico')
+next_button_img = resource_path('./images/nextbutton.png')
+dialog_img = resource_path('./images/dialog.png')
+close_img = resource_path('./images/x.png')
 
 def press_space():
     global running, use_random_delay, delay, min_delay, max_delay
     while running:
-        next_button = pyautogui.locateCenterOnScreen('./images/nextbutton.png', grayscale=True, confidence=0.8)
+        next_button = pyautogui.locateCenterOnScreen(next_button_img, grayscale=True, confidence=0.8)
+        dialog = pyautogui.locateOnScreen(dialog_img, grayscale=True, confidence=0.8)
+        close = pyautogui.locateCenterOnScreen(close_img, grayscale=True, confidence=0.8)
         if next_button is not None:
-            pyautogui.click(next_button)
-        keyboard.press(' ')
-        keyboard.release(' ')
+            click(next_button)
+        if dialog is not None:
+            click(dialog)
+        if close is not None:
+            click(close)
         if use_random_delay:
             current_delay = random.randint(min_delay, max_delay) / 1000
         else:
@@ -36,6 +59,11 @@ def toggle_start_stop():
         status_label.config(text="Status: Stopped", fg="red")
     else:
         running = True
+        hsr_windows = gw.getWindowsWithTitle("Honkai: Star Rail")
+        if hsr_windows:
+            hsr_windows[0].activate()
+        else:
+            print("Honkai: Star Rail not found!")
         threading.Thread(target=press_space, daemon=True).start()
         status_label.config(text="Status: Running", fg="green")
 
@@ -60,6 +88,7 @@ def toggle_random_delay():
     use_random_delay = random_delay_var.get()
 
 root = tk.Tk()
+root.iconbitmap(icon)
 root.title("HSR Auto")
 root.geometry("400x400")
 
@@ -70,7 +99,7 @@ status_label.pack(pady=10)
 
 tk.Label(root, text="Delay (ms):", font=("Arial", 10)).pack(pady=5)
 delay_entry = tk.Entry(root)
-delay_entry.insert(0, "500")
+delay_entry.insert(0, "100")
 delay_entry.pack(pady=5)
 
 random_delay_var = tk.BooleanVar()
@@ -84,7 +113,7 @@ min_delay_entry.pack(pady=5)
 
 tk.Label(root, text="Max Delay (ms):", font=("Arial", 10)).pack(pady=5)
 max_delay_entry = tk.Entry(root)
-max_delay_entry.insert(0, "1000")
+max_delay_entry.insert(0, "500")
 max_delay_entry.pack(pady=5)
 
 update_button = tk.Button(root, text="Update Delay", command=update_delay, bg="blue", fg="white")
